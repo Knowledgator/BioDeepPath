63e6edc188097adffb4d33da6719e6c977c9a5dc
hazemessamm <hazemessam199@gmail.com>
2023-04-29 03:55:35 +0300

    fixed bug in the training code, the original author did not add zero_grad after each step, converted the argument parser into a yaml file that contains the configuration.

---
+++ create_config.py
@@ -0,0 +1,57 @@
+import os
+
+import yaml
+from ml_collections import ConfigDict
+
+
+def create_config_dict():
+    config_dict = ConfigDict(dict(
+        transE_embed_dim=100,
+        state_dim=200,
+        action_space=1345,
+        eps_start=1,
+        eps_end=0.1,
+        epe_decay=1000,
+        replay_memory_size=10000,
+        batch_size=128,
+        embedding_dim=100,
+        gamma=0.99,
+        target_update_freq=1000,
+        max_steps=50,
+        max_steps_test=50,
+        num_episods=2000,
+        device='cuda:0',
+        task='supervised',
+    ))
+
+    return config_dict
+
+
+def create_arg_parser():
+    from argparse import ArgumentParser
+    argparser = ArgumentParser()
+    argparser.add_argument('--overwrite', default=False, type=bool)
+    return argparser
+
+
+def write_config_file(config_file_name):
+    config_dict = create_config_dict()
+    with open(config_file_name, 'w') as f:
+        yaml.dump(config_dict, f)
+    print(f'{config_file_name} created.')
+
+
+def to_yaml(overwrite=False):
+    config_file_name = 'config.yaml'
+    if os.path.exists(config_file_name):
+        if overwrite:
+            write_config_file(config_file_name)
+        else:
+            print(f'{config_file_name} already exists.')
+    else:
+        write_config_file(config_file_name)
+
+
+if __name__ == '__main__':
+    args = create_arg_parser().parse_args()
+    to_yaml(args.overwrite)
--- train.py
+++ train.py
@@ -1,8 +1,10 @@
-from argparse import ArgumentParser
+import os
+import random
 from itertools import count
 
 import numpy as np
 import torch
+import yaml
 from torch import nn, optim
 from torch.nn import functional as F
 from torch.utils import data
@@ -13,6 +15,11 @@ from tqdm.auto import tqdm
 from environment import Env
 from networks import PolicyNNV2
 from utils import Transition, construct_graph
+
+seed = 7
+torch.manual_seed(seed)
+np.random.seed(seed)
+random.seed(seed)
 
 
 class PolicyNetwork(nn.Module):
@@ -80,67 +87,65 @@ def train_supervised(policy_model, env, train_ds, num_epochs, device="cuda"):
     return policy_model
 
 
-def train_rl(
-    policy_model, env, num_episodes, max_steps, action_space, device="cuda"
-):
+def train_rl(policy_model: PolicyNetwork, env: Env, train_ds: data.Dataset,
+             num_episodes: int, max_steps: int,
+             action_space: int, device: str = 'cuda'):
     done = False
     success = 0
     invalid_path = False
     policy_model = policy_model.train()
-    for episode in range(1, num_episodes + 1):
+    sampled_indices = set()
+    for episode in range(1, num_episodes+1):        
         state_batch_negative = []
         action_batch_negative = []
         episodes = []
-        episode_path = ""
+        episode_path = ''
 
-        current_state = env.reset()
-        print(
-            f"Episode: {episode} - Current Start: {env.current_head} - Current End: {env.current_target}"
-        )
+        sampled_idx = random.choice(range(len(train_ds)))
+        while sampled_idx in sampled_indices:
+            sampled_idx = random.choice(range(len(train_ds)))
+
+        sampled_indices.add(sampled_idx)
+        entity_1, entity_2 = train_ds[sampled_idx][:-1]
+        current_state = env.reset_from(entity_1, entity_2)
+        print(f"Episode: {episode} - Current Start: {env.current_head} - Current End: {env.current_target}")
         for step in count(1):
-            episode_path += f"{env.current_head} -> "
+            episode_path += f'{env.current_head} -> '
             action_probs = policy_model(current_state.to(device))
-            chosen_relation = np.random.choice(
-                np.arange(action_space),
-                p=np.squeeze(action_probs.cpu().detach().numpy()),
-            )
+            chosen_relation = np.random.choice(np.arange(action_space),
+                                               p=np.squeeze(action_probs.cpu().detach().numpy()))
             next_state, reward, done, invalid_path = env.step(chosen_relation)
 
             if reward == -1:
                 state_batch_negative.append(current_state)
                 action_batch_negative.append(chosen_relation)
 
-            episodes.append(
-                Transition(
-                    state=current_state,
-                    action=chosen_relation,
-                    next_state=next_state,
-                    reward=reward,
-                )
-            )
+            episodes.append(Transition(state=current_state,
+                                       action=chosen_relation,
+                                       next_state=next_state, reward=reward))
             if done or step == max_steps:
-                episode_path += f"{env.current_target}"
-                print("Episode Path:", episode_path)
+                episode_path += f'{env.current_target}'
+                print('Episode Path:', episode_path)
                 break
 
             current_state = next_state
 
         if len(state_batch_negative) != 0:
-            print("Penalty to invalid steps:", len(state_batch_negative))
+            print('Penalty to invalid steps:', len(state_batch_negative))
             state_batch_negative = torch.cat(state_batch_negative).to(device)
-            action_batch_negative = torch.tensor(
-                action_batch_negative, dtype=torch.long, device=device
-            )
+            action_batch_negative = torch.tensor(action_batch_negative,
+                                                 dtype=torch.long,
+                                                 device=device)
+            policy_model.optimizer.zero_grad()
             predictions = policy_model(state_batch_negative)
-            loss = policy_model.compute_loss_rl(
-                predictions, -0.05, action_batch_negative
-            )
+            loss = policy_model.compute_loss_rl(predictions, -0.05,
+                                                action_batch_negative)
             loss.backward()
             policy_model.optimizer.step()
 
         # If the agent success, do one optimization
         if not invalid_path:
-            print("Success")
+            print('Success')
 
             success += 1
             path_length = len(env.path)
@@ -149,18 +154,17 @@ def train_rl(
             total_reward = 0.1 * global_reward + 0.9 * length_reward
             state_batch = []
             action_batch = []
-            for t, transition in enumerate(episodes):
+            for transition in episodes:
                 if transition.reward == 0:
                     state_batch.append(transition.state)
                     action_batch.append(transition.action)
             state_batch = torch.cat(state_batch).to(device)
-            action_batch = torch.tensor(
-                action_batch, device=device, dtype=torch.long
-            )
+            action_batch = torch.tensor(action_batch, device=device,
+                                        dtype=torch.long)
+            policy_model.optimizer.zero_grad()
             predictions = policy_model(state_batch)
-            loss = policy_model.compute_loss_rl(
-                predictions, total_reward, action_batch
-            )
+            loss = policy_model.compute_loss_rl(predictions, total_reward,
+                                                action_batch)
             loss.backward()
             policy_model.optimizer.step()
         else:
@@ -168,95 +172,61 @@ def train_rl(
             state_batch = []
             action_batch = []
             total_reward = global_reward
-            for t, transition in enumerate(episodes):
+            for transition in episodes:
                 if transition.reward == 0:
                     state_batch.append(transition.state)
                     action_batch.append(transition.action)
             if len(state_batch) == 0:
                 continue
             state_batch = torch.cat(state_batch).to(device)
-            action_batch = torch.tensor(
-                action_batch, device=device, dtype=torch.long
-            )
+            action_batch = torch.tensor(action_batch, device=device,
+                                        dtype=torch.long)
+            policy_model.optimizer.zero_grad()
             predictions = policy_model(state_batch)
-            loss = policy_model.compute_loss_rl(
-                predictions, total_reward, action_batch
-            )
+            loss = policy_model.compute_loss_rl(predictions, total_reward,
+                                                action_batch)
             loss.backward()
             policy_model.optimizer.step()
 
-            print("Failed, Do one teacher guideline")
-            good_episodes = env.generate_episodes(
-                env.initial_head, env.current_target, 1
-            )
+            print('Failed, Do one teacher guideline')
+            good_episodes = env.generate_episodes(env.initial_head,
+                                                  env.current_target, 1)
             for item in good_episodes:
                 teacher_state_batch = []
                 teacher_action_batch = []
                 total_reward = 0.7 * 1 + 0.3 * len(item)
-                for t, transition in enumerate(item):
+                for transition in item:
                     teacher_state_batch.append(transition.state)
                     teacher_action_batch.append(transition.action)
 
-                teacher_state_batch = (
-                    torch.cat(teacher_state_batch)
-                    .squeeze()
-                    .to(device=device, dtype=torch.float32)
-                )
-                teacher_action_batch = torch.tensor(teacher_action_batch).to(
-                    device=device, dtype=torch.long
-                )
+                teacher_state_batch = torch.cat(teacher_state_batch).squeeze().to(device=device, dtype=torch.float32)
+                teacher_action_batch = torch.tensor(teacher_action_batch).to(device=device, dtype=torch.long)
+                policy_model.optimizer.zero_grad()
                 predictions = policy_model(teacher_state_batch)
-                loss = policy_model.compute_loss_rl(
-                    predictions, total_reward, teacher_action_batch
-                )
+                loss = policy_model.compute_loss_rl(predictions, 1,
+                                                    teacher_action_batch)
                 loss.backward()
                 policy_model.optimizer.step()
 
-    torch.save(policy_model.state_dict(), "policy_rl_weights.pt")
-    torch.save(policy_model.optimizer.state_dict(), "policy_optimizer_rl.pt")
+    torch.save(policy_model.state_dict(), 'policy_rl_weights_v3.pt')
+    torch.save(policy_model.optimizer.state_dict(), 'policy_optimizer_rl_v3.pt')
     return policy_model
 
 
-def create_arg_parser():
-    argparser = ArgumentParser()
-    argparser.add_argument("--transE_embed_dim", default=100, type=int)
-    argparser.add_argument("--state_dim", required=False, default=200, type=int)
-    argparser.add_argument(
-        "--action_space", required=False, default=1345, type=int
-    )
-    argparser.add_argument("--eps_start", required=False, default=1, type=int)
-    argparser.add_argument("--eps_end", required=False, default=0.1, type=float)
-    argparser.add_argument(
-        "--epe_decay", required=False, default=1000, type=int
-    )
-    argparser.add_argument(
-        "--replay_memory_size", required=False, default=10000, type=int
-    )
-    argparser.add_argument(
-        "--batch_size", required=False, default=128, type=int
-    )
-    argparser.add_argument(
-        "--embedding_dim", required=False, default=100, type=int
-    )
-    argparser.add_argument("--gamma", required=False, default=0.99, type=float)
-    argparser.add_argument(
-        "--target_update_freq", required=False, default=1000, type=int
-    )
-    argparser.add_argument("--max_steps", required=False, default=50, type=int)
-    argparser.add_argument(
-        "--max_steps_test", required=False, default=50, type=int
-    )
-    argparser.add_argument(
-        "--num_episods", required=False, default=2000, type=int
-    )
-    argparser.add_argument(
-        "--device", required=False, default="cuda:0", type=str
-    )
-    return argparser
+def read_config_file(config_file_name):
+    if not os.path.exists(config_file_name):
+        raise FileNotFoundError(f'{config_file_name} does not exist. '
+                                'please call `create_config.py` first '
+                                'using `python create_config.py`.')
+
+    with open(config_file_name, 'r') as f:
+        config = yaml.load(f, yaml.UnsafeLoader)
+    return config
+
 
 
 if __name__ == "__main__":
-    args = create_arg_parser().parse_args()
+    args = read_config_file('config.yaml')
     kg_train, _, _ = load_fb15k()
     model = TransEModel(
         args.transE_embed_dim,
@@ -265,7 +235,13 @@ if __name__ == "__main__":
         dissimilarity_type="L2",
     )
     G = construct_graph(kg_train)
-    env = Env(G, kg_train, model, kg_train.relations.tolist())
+    env = Env(G, model)
     policy = PolicyNetwork(args.state_dim, args.action_space).to(args.device)
-    train_supervised(policy, env, kg_train, 2)
-    train_rl(policy, env, kg_train, args.num_episods, args.max_steps)
+    if args.task == 'supervised':
+        train_supervised(policy, env, kg_train, 2)
+    elif args.task == 'rl':
+        train_rl(policy, env, kg_train, args.num_episods, args.max_steps,
+                 args.action_space)
+    else:
+        raise ValueError('Unknown task, expected `supervised` or `rl` task. '
+                         f'Recieved: {args.task}.')
